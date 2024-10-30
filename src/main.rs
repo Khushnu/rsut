03f060a83af4bc4169b9ab05763cfb4324b56e25
Waleed Ahmad <34332682+Khushnu@users.noreply.github.com>
2024-10-31 01:06:03 +0500

    updated upto chapter 6 enums

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,195 @@
 
 //the rand::Rng  the rand is an object to access the library functions.
 
+// struct  User{
+//     active: bool, 
+//     username: String, 
+//     email: String
+// }
+
+// fn build_user(email: String, username: String) -> User {
+//     User{
+//         active: true, 
+//          email, 
+//         username,
+//     }
+// }
+
+// fn area(width: u32, height: u32) -> u32 {
+//     width * height
+// }
+
+//to make it more readable . use the tuple in side there. so from the dimension 
+//get the dimension.0 , and dimention.1; 
+// fn area(dimension: (u32, u32)) -> u32{
+//     dimension.0 * dimension.1
+// }
+
+//another way using reference 
+// struct Rectangle {
+//     height: u32, 
+//     width: u32
+// }
+
+//Storing values directly inside the enums for more concise 
+//it also Store different typeof data  one is string which i use 
+//diffrent type of data 
+enum IpAddrKind {
+    // V4(String),
+    V4(u8, u8,u8, u8),
+    V6(String),
+}
+
+// struct Circle {
+//     width: u32, 
+//     height: u32
+// }
+//why use reference because we want to use its field without taking the ownership
+// fn area(dimension: &Rectangle) -> u32 {
+//     dimension.height * dimension.width
+// }
+
+//Method Syntax 
+// impl Rectangle{
+//     fn area(&self) -> u32{
+//         self.height * self.width
+//     }
+// }
+
+// impl Rectangle {
+//     fn square(size: u32)-> Self{
+//         Self{
+//             width: size, 
+//             height: size
+//         }
+//     }
+//}
+
+// impl Circle {
+//     fn sc(&self) -> u32{
+//         self.height * self.width
+//     }
+// }
+
+// struct IpAddress{
+//     kind: IpAddrKind, 
+//     address: String
+// }
+
+//multiple struct under one message 
+// enum Message {
+//     Quit, // no store data 
+//     Move {x: i32, y: i32}, //annoymous struct bcuz no name is found 
+//     Write (String), 
+//     ChangeColor (i32, i32, i32)
+// }
+// impl Message {
+//     fn call(&self) {
+//         match  self {
+//             Message::Quit => println!("Quite Struct"), 
+//             Message::Write(text) => println!("Text from Write {}", text), 
+//             Message::Move { x, y } => println!("x value is {} y Value is {}", x ,y), 
+//             _ => println!("default")
+//         }
+//     }
+
+//     // let m = Message::Write(String::from(""));
+// }
+
+// fn route(ip_kind: IpAddrKind){}
 
 fn main() {
+
+    // let four = IpAddrKind::V4;
+    // let six = IpAddrKind::V6;
+
+    // let s = Message::call(&Message::Quit);
+
+    //let localHost = IpAddrKind::V4(String::from("127.0.0.1")); this one for string
+    // let localHost = IpAddrKind::V4(120, 0, 0, 1); 
+
+
+
+    // let localHost = IpAddress{
+    //     kind: IpAddrKind::V4, 
+    //     address: String::from("127.0.0.1")
+    // };
+
+
+
+
+    // let rect1 = Rectangle{
+    //     height : 12, 
+    //     width : 4
+    // }; 
+
+    // let s = Rectangle::square(3);
+
+    // println!("Rectangle are is {}",s.height )
+
+    // let rectangle = Rectangle {
+    //     height: 20, 
+    //     width: 5
+    // };
+
+    //here we create a tuple to make it more readable 
+    // let rect = (30,40);
+
+    // let height = 32; 
+    // let width = 4;
+
+    // println!("The area of the rectangle is {} pixels", area(width, height))
+    //here we also create the reference to rectangle.
+    // println!("The area of the rectangle is {} pixels", area(&rectangle))
+    // println!("The area of the rectangle is {} pixels", area(rect))
+
+//    let g = build_user(String::from(" Email "), String::from("Waleed UserNAme"));
+//     let user1 = User{
+//         active : true, 
+//         email: String::from("emaill"), 
+//         username: String::from("username")
+//     };
+
+//     let user2 = User{
+//         active: false, 
+//         email: String::from("EmailUser2"), 
+//         ..user1
+//     };
+
+
+//     println!("{}", g.username)
+
+    // let user1 = User{
+    //     active: true, 
+    //     email: String::from("Waleed@gmail.com"),
+    //     username: String::from("Waleed_ahmad")
+    // };
+
+    // let g = user1.email;
+    // let s = user1.username;
+    
+    // print!("{g}");
+    // let a = [12,32,3,42];
+    // let slice = &a[1..4];
+    // println!("{:?}", slice);
+    //sliceing without taking ownership 
+    // let mut arr = [1,443,2,123,123,53,9,34];
+    // let slices = &mut arr[0..5];
+    // print!("{:?}", slices);
+    // let mut g = "fdf";
+    // g = "asd";
+    // println!("{g}");
+    // let mut s = String::from("Hello World");
+    // let word = first_word(&s);
+    // print!("{word}");
+    // s.clear();
+        // println!("{}",g);
+    
+
+
+    // s.push_str(" hello ,World ");
+
+    // println!("{s}")
 
     // let arr = [12,434,53,532];
     //using for loop  iter use to give iteration of every element 
@@ -236,3 +423,31 @@ fn main() {
 //second .. 5 that's it 
 
 //}
+
+
+//what happens if we reference toward invalid data 
+// function attempts to return a reference to a local variable that is 
+// dropped when the function exits, which is not allowed in Rust. You can 
+// either return the owned value directly or return a reference that points 
+// to data with a longer lifetime. This strict borrowing and ownership system 
+// is one of the key features of Rust that helps prevent memory safety issues 
+// like dangling
+// fn dangle() -> &String {
+//     let s = String::from("hello");
+//      &s
+// }
+
+//SLices 
+// A slice is a reference to a contiguous portion of a collection, such as an array or a vector.
+// Slices are used when you want to access a subset of an array or string without needing to copy or own the entire collection.
+// The syntax for creating a slice involves using a range syntax with the collection, such as &array[start..end].
+// fn first_word(s: &String) -> usize {
+//     let bytes = s.as_bytes();
+
+//     for(i, &items) in bytes.iter().enumerate(){
+//         if items == b' '{
+//             return  i;
+//         } 
+//     }
+//     s.len()
+// }
